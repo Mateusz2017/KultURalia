61206260b7d7032b5a7f050b996a5b97e41e3013
kuba3351 <kuba3351@gmail.com>
2016-04-29 14:46:15 +0200

    added fanpage link and authors page

--- app/src/main/java/pl/edu/ur/kulturalia/Home.java
+++ app/src/main/java/pl/edu/ur/kulturalia/Home.java
@@ -1,13 +1,43 @@
 package pl.edu.ur.kulturalia;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    Button fanpage;
+    Button autorzy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        fanpage = (Button) findViewById(R.id.button5);
+        fanpage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/kultURaliaUR"));
+                    v.getContext().startActivity(Intent.createChooser(intent, "Wybór przeglądarki"));
+                }
+                catch(Exception e)
+                {
+                    Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        autorzy = (Button) findViewById(R.id.button4);
+        autorzy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Autorzy.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 }
