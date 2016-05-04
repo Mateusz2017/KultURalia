56a740f9a50ec6e87a89a43cf016b0dc4c27ac38
Y0Men <gwatek@gmail.com>
2016-05-04 22:52:28 +0200

    add fade animation

--- app/src/main/java/pl/edu/ur/kulturalia/Intro.java
+++ app/src/main/java/pl/edu/ur/kulturalia/Intro.java
@@ -1,15 +1,39 @@
 package pl.edu.ur.kulturalia;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Intro extends AppCompatActivity {
 
+    /*CountDownTimer cTimer = null;
+
+    public void startLogoTimer() {
+
+        cTimer = new CountDownTimer(5000, 100) {
+            public void onTick(long millisUntilFinished) {
+
+                logo.setAlpha(millisUntilFinished/50000f);
+            }
+            public void onFinish() {
+            }
+        };
+        cTimer.start();
+    }
+    public void cancelTimer() {
+        if(cTimer!=null)
+            cTimer.cancel();*/
+
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getSupportActionBar().hide(); //ukrywamy górny pasek tytułu
@@ -19,6 +43,9 @@ public class Intro extends AppCompatActivity {
             public void run() {
                 try {
                     Thread.sleep(5000); //opóźnienie startu menu w milisekundach
+                    ImageView logo = (ImageView) findViewById(R.id.logo);
+                    Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                    logo.startAnimation(animation1);
                 }
                 catch(Exception e)
                 {
