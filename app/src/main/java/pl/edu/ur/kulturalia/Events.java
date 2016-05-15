90283b2b63d8f7f0f4615a3bff09a1a6d70724a6
Tymek <tymekjw@gmail.com>
2016-05-15 10:36:24 +0200

    Eventy.

---
+++ app/src/main/java/pl/edu/ur/kulturalia/Events.java
@@ -0,0 +1,35 @@
+package pl.edu.ur.kulturalia;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class Events extends Activity {
+
+    private WebView mWebview ;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mWebview .loadUrl("http://www.facebook.com/kultURaliaUR/events");
+        setContentView(mWebview );
+
+    }
+
+}
