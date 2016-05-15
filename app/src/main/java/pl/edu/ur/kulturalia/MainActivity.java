2ae1234c67a14ec8cbcbe961ac7b92b27092e7cf
Tymek <tymekjw@gmail.com>
2016-05-15 10:32:34 +0200

    Niewielkie zmiany.

--- app/src/main/java/pl/edu/ur/kulturalia/MainActivity.java
+++ app/src/main/java/pl/edu/ur/kulturalia/MainActivity.java
@@ -97,8 +97,11 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(this,"Jak do nas dojechać?", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, Dojazd.class);
             startActivity(intent);
+
         } else if (id == R.id.eventy) {
             Toast.makeText(this,"Eventy KultURaliowe", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, Events.class);
+            startActivity(intent);
 
         } else if (id == R.id.sponsorzy) {
             Toast.makeText(this,"Sponsorzy KultURaliów ", Toast.LENGTH_SHORT).show();
